1261206f815e42e853a1ee08515c2b3ac41ed0c4
gizmore <gizmore@wechall.net>
2020-11-13 07:46:03 +0100

    6.10-r9375_onInitSidebar

--- Module_Sitemap.php
+++ Module_Sitemap.php
@@ -2,15 +2,16 @@
 namespace GDO\Sitemap;
 
 use GDO\Core\GDO_Module;
-use GDO\UI\GDT_Bar;
 use GDO\UI\GDT_Link;
+use GDO\DB\GDT_Checkbox;
+use GDO\UI\GDT_Page;
 
 /**
  * Build a sitemap reachable from the bottom bar.
  * 
  * @author gizmore
- * @since 6.10
  * @version 6.10
+ * @since 6.10
  */
 final class Module_Sitemap extends GDO_Module
 {
@@ -19,11 +20,23 @@ final class Module_Sitemap extends GDO_Module
 	##############
 	public function onLoadLanguage() { return $this->loadLanguage('lang/sitemap'); }
 	
+	public function getConfig()
+	{
+	    return [
+	        GDT_Checkbox::make('bottom_bar')->initial('1'),
+	    ];
+	}
+	public function cfgBottomBar() { return $this->getConfigValue('bottom_bar'); }
+	
 	#############
 	### Hooks ###
 	#############
-	public function hookBottomBar(GDT_Bar $bar)
+	public function onInitSidebar()
 	{
-		$bar->addField(GDT_Link::make('link_sitemap')->href($this->href('Show')));
+	    if ($this->cfgBottomBar())
+	    {
+	        GDT_Page::$INSTANCE->bottomNav->addField(GDT_Link::make('link_sitemap')->href($this->href('Show')));
+	    }
 	}
+
 }
