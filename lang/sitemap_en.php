3278b41e784fa2260520f232e80c5ca2b1acbc81
gizmore <gizmore@wechall.net>
2021-05-08 04:58:40 +0200

    6.10.2-r1082_tbs_migrate

--- lang/sitemap_en.php
+++ lang/sitemap_en.php
@@ -1,5 +1,5 @@
 <?php
 return array(
 'link_sitemap' => 'Sitemap',
-'mdescr_sitemap_show' => 'Sitemap for ' . GWF_SITENAME . ' / ' . GWF_DOMAIN,
+'mdescr_sitemap_show' => 'Sitemap for ' . GDO_SITENAME . ' / ' . GDO_DOMAIN,
 );
